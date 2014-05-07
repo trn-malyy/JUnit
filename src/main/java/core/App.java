19a7181e7f8d7f8804878822b046c33db4ac07b6
Serhiy Malyy <serhiy.malyy@gmail.com>
2014-05-06 22:17:07 -0700

    JUnit Assertions & Surefire Report

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -6,8 +6,15 @@ package core;
  */
 public class App 
 {
+
+public static String s = "Testing JUnit 4";
+public static Integer i = 55;
+public static Boolean t = true;
+public static Boolean f = false;
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Application was called" );
     }
 }
--- src/test/java/core/AppTest.java
+++ src/test/java/core/AppTest.java
@@ -32,55 +32,61 @@ public class AppTest {
 	 
 	@Test
 	public void Test_01_assertEquals_Positive() {
-	       System.out.println("Test_01_assertEquals_Positive");
+	       //System.out.println("Test_01_assertEquals_Positive");
+		assertEquals("String not the same", "Testing JUnit 4", App.s);
 	}	
 	
 	@Test
 	public void Test_02_assertEquals_Negative() {
-	       System.out.println("Test_02_assertEquals_Negative");
+	       //System.out.println("Test_02_assertEquals_Negative");
+		assertEquals("String not the same.", "Testing TestNG 6", App.s);
        }
 	@Ignore
 	@Test
 	public void Test_03_assertEquals_Ignored() {
-	       System.out.println("Test_03_assertEquals_Ignored");
+	       //System.out.println("Test_03_assertEquals_Ignored");
+		assertEquals("String not the same.", "Testing JUnit 3", App.s);
 	       }
 	@Test
 	public void Test_04_AssertSame_Positive() {
-	       System.out.println("Test_04_AssertSame_Positive");
+	       //System.out.println("Test_04_AssertSame_Positive");
+		assertSame("Integer not the same.", 55,App.i);
 	       }
 	 
 	@Test
 	public void Test_05_AssertSame_Negative() {
-	       System.out.println("Test_05_AssertSame_Negative");
+	       //System.out.println("Test_05_AssertSame_Negative");
+		assertSame("Integer not the same.", 54,App.i);
        }
 	
 	@Ignore
 	@Test
    public void Test_06_AssertSame_Ignored() {
-	       System.out.println("Test_06_AssertSame_Ignored");
+	       //System.out.println("Test_06_AssertSame_Ignored");
+		assertSame("Integer not the same.", 56,App.i);
 	       }
 	@Test
 	public void Test_07_assertFalse_Positive() {
 	
-	       System.out.println("Test_07_assertFalse_Positive");
+	       //System.out.println("Test_07_assertFalse_Positive");
+		assertFalse("Boolean should be false", App.f);
 	       }
 	@Test
 	
 	public void Test_08_assertFalse_Negative() {
-	       System.out.println("Test_08_assertFalse_Negative");
+	       //System.out.println("Test_08_assertFalse_Negative");
+		assertFalse("Boolean should be false", App.t);
 	       }
 	@Test
 	public void Test_09_assertTrue_Positive() {
-       System.out.println("Test_09_assertTrue_Positive");
+       //System.out.println("Test_09_assertTrue_Positive");
+		assertTrue("Boolean should be true", App.t);
+       
 	       }
 	@Test
 	public void Test_10_assertTrue_Negative() {
-	       System.out.println("Test_10_assertTrue_Negative");
+	       //System.out.println("Test_10_assertTrue_Negative");
+		assertTrue("Boolean should be true", App.f);
 	       }
-	
-//	@Test
-//	public void test() {
-//		//fail("Not yet implemented");
-//	}
 
 }
